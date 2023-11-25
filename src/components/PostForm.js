3a162a53f83955b375815b02bbcffaeea3efd260
Hülya Nergiz <akin.hulya@outlook.com>
2023-11-25 21:32:02 +0300

    adds notes

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,8 +3,11 @@ import { useForm } from "react-hook-form";
 import { nanoid } from "nanoid";
 import { useHistory } from "react-router";
 import Gratitude from "./../assets/grForm.png";
+import { notEkleAPI } from "../actions";
+import { useDispatch } from "react-redux";
 
 export default function PostForm() {
+  const dispatch = useDispatch();
   const {
     register,
     handleSubmit,
@@ -21,11 +24,12 @@ export default function PostForm() {
         .filter((v) => v !== "")
         .join("|"),
     };
+    dispatch(notEkleAPI(yeniNot));
 
     // burada ilgili eylemi dispatch edin
     // toast mesajı gösterin
     // sonra aşağıdaki satırı aktifleştirin
-    // setTimeout(() => history.push("/notlar"), 2000);
+    setTimeout(() => history.push("/notlar"), 2000);
   }
 
   const inputCx = "border border-zinc-300 h-9 rounded-none text-sm px-2 w-full";
@@ -35,7 +39,6 @@ export default function PostForm() {
       <div className="flex-1">
         <img src={Gratitude} alt="" className="block object-cover h-full" />
       </div>
-
 
       <form
         onSubmit={handleSubmit(onSubmit)}
@@ -48,8 +51,8 @@ export default function PostForm() {
           yansıtmalara kadar pek çok şeyden oluşabilir.
         </p>
         <p className="text-stone-700 my-3 text-xs">
-          Her gün belli saatlerde 3 maddeden oluşan bir liste
-          yapmak, bu alışkanlığa iyi bir başlangıç noktası sayılır.
+          Her gün belli saatlerde 3 maddeden oluşan bir liste yapmak, bu
+          alışkanlığa iyi bir başlangıç noktası sayılır.
         </p>
         <div>
           <input
@@ -76,10 +79,7 @@ export default function PostForm() {
           />
         </div>
 
-        <button
-          type="submit"
-          className="myButton"
-        >
+        <button type="submit" className="myButton">
           Ekle
         </button>
       </form>
