dc5b2fb14c040a2d0adb7df2840733b2a2046599
Hülya Nergiz <akin.hulya@outlook.com>
2023-11-25 16:33:58 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import logger from "react-logger";
+import thunk from "redux-thunk";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
